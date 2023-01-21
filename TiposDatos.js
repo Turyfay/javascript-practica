803f3b1ee37e42133d5c629c974fafbd3fc21e1a
David Enamorado <david8enaceituno@gmail.com>
2023-01-21 01:21:44 -0600

    feat: concatenacion de strings

---
+++ TiposDatos.js
@@ -0,0 +1,11 @@
+//Tipo de datos string
+var nombre = "Carlos";
+console.log(nombre);
+
+//Concatenación de strings
+/* Las expresión se evalúan de izquierda a derecha  */
+var x = nombre + 2;
+var y = 2 + nombre;
+var z = 2 + 2 + nombre;
+var a = nombre + ( 2 + 2);
+console.log(x,y,z,a);
